33d7702221a2cd8afa6beeb3d582c647440eb2e1
Michael Neuroth <michael.neuroth@freenet.de>
2016-11-09 22:31:23 +0100

    Update build files

--- build_android.bat
+++ build_android.bat
@@ -1,8 +1,7 @@
-rem !/bin/sh
-rem setmingw.bat --> for mingw32-make
+rem call ..\visiscript\set_android_env.bat
 
 set NDK_ROOT=C:\usr\android-ndk-r11c
-set PATH=%NDK_ROOT%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin:%PATH%
+set PATH=%NDK_ROOT%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin;%PATH%
 set SYS_ROOT=%NDK_ROOT%\platforms\android-9\arch-arm
 set CC=arm-linux-androideabi-g++ --sysroot=C:\usr\android-ndk-r11c\platforms\android-9\arch-arm
 set LD=arm-linux-androideabi-ld
@@ -16,7 +15,7 @@ set CFLAGS=-fPIC --sysroot=%SYS_ROOT%
 set LDFLAGS=-pie
 
 del *.o
-mingw32-make -f makefile.unx -e android_win=1 release=1
+mingw32-make -j 4 -f makefile.unx -e android_win=1 release=1
 copy minscript minscript_O2_android
 C:\usr\android-ndk-r11c\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin\arm-linux-androideabi-strip minscript_O2_android
 rem del *.o
